4166d3e1d2cdf45d881b2b6263a1d5d9ea75a184
Adrian Frankowski <adrian.frankowski95@gmail.com>
2025-01-28 00:33:18 +0100

    Add partially implemented events processor

--- src/eShop.DomainDrivenDesign/EventHandling.fs
+++ src/eShop.DomainDrivenDesign/EventHandling.fs
@@ -1,7 +1,10 @@
 ﻿namespace eShop.DomainDrivenDesign
 
 open System
+open System.Threading.Tasks
+open Microsoft.Extensions.Logging
 open eShop.Prelude
+open FsToolkit.ErrorHandling
 
 type Event<'payload> =
     { Data: 'payload; OccurredAt: DateTimeOffset }
@@ -11,7 +14,6 @@ type EventHandler<'eventPayload, 'ioError> = Event<'eventPayload> -> AsyncResult
 
 
 type HandlerName = string
-
 type AggregateTypeName = string
 
 
@@ -21,7 +23,60 @@ module EventHandlerRegistry =
     let empty<'eventPayload, 'ioError>: EventHandlerRegistry<'eventPayload, 'ioError> =
         Map.empty |> EventHandlerRegistry
         
-    let register<'state, 'eventPayload, 'ioError> handlerName (handler: EventHandler<'eventPayload, 'ioError>) registry=
+    let register<'state, 'eventPayload, 'ioError> handlerName (handler: EventHandler<'eventPayload, 'ioError>) (EventHandlerRegistry registry)=
         let aggregateName = typedefof<'state>.Name
         
-        registry |> Map.add (aggregateName, handlerName) handler
+        registry |> Map.add (aggregateName, handlerName) handler |> EventHandlerRegistry
+
+
+[<RequireQualifiedAccess>]
+module EventsProcessor =
+    type private Delay = TimeSpan
+    type Attempt = int
+    
+    type EventsProcessorOptions<'state, 'eventPayload, 'ioError> =
+        private
+            { EventHandlerRegistry: EventHandlerRegistry<'eventPayload, 'ioError>; Retries: Delay list }
+            
+    let init<'state, 'eventPayload, 'ioError>: EventsProcessorOptions<'state, 'eventPayload, 'ioError> =
+        { EventHandlerRegistry = EventHandlerRegistry.empty
+          Retries =
+              [ TimeSpan.FromMinutes(1: float)
+                TimeSpan.FromMinutes(2: float)
+                TimeSpan.FromMinutes(3: float)
+                TimeSpan.FromMinutes(20: float) ] }
+        
+    let withRetries retries options = { options with Retries = retries }
+    
+    let registerHandler<'state, 'eventPayload, 'ioError>
+        handlerName
+        (handler: EventHandler<'eventPayload, 'ioError>)
+        (options: EventsProcessorOptions<'state, 'eventPayload, 'ioError>)
+        =
+            { options with EventHandlerRegistry = options.EventHandlerRegistry |> EventHandlerRegistry.register<'state, 'eventPayload, 'ioError> handlerName handler }
+            
+    type private Command<'eventPayload, 'ioError> =
+        | HandleEvents of Event<'eventPayload> list
+        | TriggerRetry of Attempt * (Event<'eventPayload> * EventHandler<'eventPayload, 'ioError> list)
+        
+    type T<'state, 'eventPayload, 'ioError> internal (logger: ILogger, options: EventsProcessorOptions<'state, 'eventPayload, 'ioError>) =
+        let scheduleRetry reply attempt eventAndFailedHandlers =
+            task {
+                do!
+                    options.Retries
+                    |> List.item (attempt - 1)
+                    |> Task.Delay
+                    
+                return (attempt, eventAndFailedHandlers) |> TriggerRetry |> reply
+            }
+            
+        let handleEventsAndCollectFailedHandlers (handlers: EventHandler<'eventPayload, 'ioError> list) attempt event =
+            async {
+                let! failedHandlers =
+                    handlers
+                    |> List.traverseAsyncResultA (fun handler ->
+                        event
+                        |> handler
+                        |> 
+                        )
+            }
