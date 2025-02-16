0b45b06657cd41e87159b6fe7775d8760753fb1b
Adrian Frankowski <adrian.frankowski95@gmail.com>
2025-02-16 20:37:30 +0100

    Add eShop.DomainDrivenDesign.Postgres fsproj

---
+++ src/eShop.DomainDrivenDesign.Postgres/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace eShop.DomainDrivenDesign.Postgres
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/eShop.DomainDrivenDesign.Postgres/dbinit/Create_EventLog_Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE SCHEMA IF NOT EXISTS "eShop";
+
+CREATE TABLE "eShop"."EventLog"
+(
+    "Id"                      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "AggregateType"           VARCHAR(50) NOT NULL,
+    "Data"                    JSONB       NOT NULL,
+    "OccurredAt"              TIMESTAMPTZ NOT NULL,
+    "SuccessfulEventHandlers" TEXT[] NULL,
+    "ProcessedAt"             TIMESTAMPTZ NULL
+);
