5e92594242033b2060c91c2e0dc2fb08ce352a56
Adrian Frankowski <adrian.frankowski95@gmail.com>
2025-02-17 23:15:26 +0100

    Implement TransactionalWorkflowExecutor

--- src/eShop.DomainDrivenDesign.Postgres/TransactionalWorkflowExecutor.fs
+++ src/eShop.DomainDrivenDesign.Postgres/TransactionalWorkflowExecutor.fs
@@ -1,11 +1,58 @@
 ﻿[<RequireQualifiedAccess>]
 module eShop.DomainDrivenDesign.Postgres
 
+open System
 open System.Data
-open eShop.DomainDrivenDesign
+open System.Data.Common
+open eShop.Prelude
+open FsToolkit.ErrorHandling
 
-type WorkflowInTransaction<'command, 'state, 'event, 'domainError, 'ioError> =
-    IDbTransaction -> Workflow<'command, 'state, 'event, 'domainError, 'ioError>
+type GetDbConnection = unit -> DbConnection
 
 [<RequireQualifiedAccess>]
 module TransactionalWorkflowExecutor =
+    type private Delay = TimeSpan
+
+    type Options =
+        private
+            { IsolationLevel: IsolationLevel
+              GetDbConnection: GetDbConnection
+              Retries: Delay list }
+
+    let init getDbConnection =
+        { GetDbConnection = getDbConnection
+          IsolationLevel = IsolationLevel.ReadCommitted
+          Retries = ([ 1; 3; 5; 15 ]: float list) |> List.map TimeSpan.FromSeconds }
+
+    let withRetries retries options = { options with Retries = retries }
+
+    let withIsolationLevel isolationLevel options =
+        { options with
+            IsolationLevel = isolationLevel }
+
+    let execute workflow (options: Options) =
+        fun state command ->
+            let rec executeInTransaction workflow (retries: Delay list) =
+                asyncResult {
+                    let connection = options.GetDbConnection()
+                    do! connection.OpenAsync()
+
+                    let! transaction = connection.BeginTransactionAsync(options.IsolationLevel).AsTask()
+
+                    return!
+                        command
+                        |> workflow transaction state
+                        |> AsyncResult.teeAsync (transaction.CommitAsync >> Async.AwaitTask)
+                        |> AsyncResult.teeErrorAsync (transaction.RollbackAsync >> Async.AwaitTask)
+                        |> AsyncResult.teeUnitAsync (connection.CloseAsync >> Async.AwaitTask)
+                        |> AsyncResult.orElseWith (fun error ->
+                            match retries with
+                            | [] -> error |> AsyncResult.error
+                            | head :: tail ->
+                                asyncResult {
+                                    do! head |> Async.Sleep
+                                    return! tail |> executeInTransaction workflow
+                                })
+                }
+
+            executeInTransaction workflow options.Retries
--- src/eShop.Prelude/AsyncResult.fs
+++ src/eShop.Prelude/AsyncResult.fs
@@ -7,3 +7,40 @@ type AsyncResult<'ok, 'err> = Result<'ok, 'err> Async
 [<RequireQualifiedAccess>]
 module AsyncResult =
     let collapse x = x |> Async.map Result.collapse
+
+    let inline teeAsync ([<InlineIfLambda>] f) x =
+        async {
+            let! result = x
+
+            return!
+                match result with
+                | Ok ok ->
+                    async {
+                        do! f ok
+                        return ok |> Ok
+                    }
+                | Error err -> err |> AsyncResult.error
+        }
+
+    let inline teeErrorAsync ([<InlineIfLambda>] f) x =
+        async {
+            let! result = x
+
+            return!
+                match result with
+                | Ok ok -> ok |> AsyncResult.ok
+                | Error err ->
+                    async {
+                        do! f err
+                        return err |> Error
+                    }
+        }
+
+    let inline teeUnitAsync ([<InlineIfLambda>] f) x =
+        async {
+            let! result = x
+
+            do! f ()
+
+            return result
+        }
