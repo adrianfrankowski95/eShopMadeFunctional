f106e3bb7b55e8e10ff4d7a580235b1a522fa0f5
Adrian Frankowski <adrian.frankowski95@gmail.com>
2025-02-19 17:03:45 +0100

    Create init script template

--- src/eShop.DomainDrivenDesign.Postgres/Adapter.fs
+++ src/eShop.DomainDrivenDesign.Postgres/Adapter.fs
@@ -1,8 +1,10 @@
 ﻿namespace eShop.DomainDrivenDesign.Postgres
 
+open System
 open System.Data.Common
 open System.Threading
 open DbUp
+open DbUp.Engine
 
 type GetDbConnection = unit -> DbConnection
 
@@ -21,18 +23,35 @@ type SqlConnection =
 
 [<RequireQualifiedAccess>]
 module Db =
-    let init (scriptsPath: string) (schema: DbSchema) (connectionString: DbConnectionString) =
+    let private handleScriptExecutionResult dbName (result: DatabaseUpgradeResult) =
+        match result.Successful with
+        | true ->
+            Console.ForegroundColor <- ConsoleColor.Green
+
+            $"""Successfully initialized %s{dbName} database. Executed scripts: %s{result.Scripts |> Seq.map _.Name |> String.concat ", "}"""
+            |> Console.WriteLine
+
+            Console.ResetColor()
+        | false ->
+            Console.ForegroundColor <- ConsoleColor.Red
+
+            $"""Failed to initialize %s{dbName} database, error: %s{result.Error.Message}. Error script: %s{result.ErrorScript.Name}"""
+            |> Console.WriteLine
+
+            Console.ResetColor()
+
+    let init (dbName: string) (scriptsPath: string) (schema: DbSchema) (connectionString: DbConnectionString) =
         let rec ensureDb retries =
             try
                 EnsureDatabase.For.PostgresqlDatabase(connectionString)
             with _ ->
                 match retries with
-                | 0 -> "Cannot initialize DB - it does not exist" |> failwith
+                | 0 -> $"Cannot initialize %s{dbName} DB - it does not exist" |> failwith
                 | _ ->
                     Thread.Sleep(3000)
                     ensureDb (retries - 1)
 
-        let invokeScripts () =
+        let executeScripts () =
             DeployChanges.To
                 .PostgresqlDatabase(connectionString)
                 .WithScriptsFromFileSystem(scriptsPath)
@@ -43,4 +62,4 @@ module Db =
                 .PerformUpgrade()
 
         ensureDb 5
-        invokeScripts ()
+        executeScripts () |> handleScriptExecutionResult dbName
