9bf468acfb1fff66664588e66be7c5b2d0d171ed
Adrian Frankowski <adrian.frankowski95@gmail.com>
2025-02-23 01:27:24 +0100

    Add serialization

--- src/OrderService/Ordering.API/Configuration.fs
+++ src/OrderService/Ordering.API/Configuration.fs
@@ -1,17 +1,23 @@
 ﻿[<RequireQualifiedAccess>]
 module Ordering.API.Configuration
 
+open System
 open System.Data.Common
+open System.Text.Json
 open Giraffe
 open Microsoft.AspNetCore.Builder
-open Microsoft.Data.SqlClient
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
+open Npgsql
 open eShop.DomainDrivenDesign.Postgres
 open eShop.DomainDrivenDesign.Postgres.DependencyInjection
 open eShop.ServiceDefaults
 open eShop.EventBusRabbitMQ
 open Ordering.API.WebApp
+open System.Text.Json.Serialization
+
+let private configureSerialization (services: IServiceCollection) =
+    JsonFSharpOptions.Default().ToJsonSerializerOptions() |> services.AddSingleton
 
 let private configureDb (config: IConfiguration) (services: IServiceCollection) =
     let schema = "ordering" |> DbSchema
@@ -20,13 +26,33 @@ let private configureDb (config: IConfiguration) (services: IServiceCollection) 
     let initEventProcessingDb = Postgres.createInitScriptForEventProcessing schema
     let initOrderingDb = Postgres.createScript "Ordering" "./dbinit" schema
 
-    services.AddTransient<DbConnection>(fun _ -> new SqlConnection(connectionString))
+    services
+        .AddSingleton<NpgsqlDataSource>(
+            Func<IServiceProvider, NpgsqlDataSource>(
+                _.GetRequiredService<JsonSerializerOptions>()
+                >> NpgsqlDataSourceBuilder(connectionString).ConfigureJsonOptions
+                >> _.Build()
+            )
+        )
+        .AddTransient<DbConnection>(_.GetRequiredService<NpgsqlDataSource>().CreateConnection())
     |> addDbSchema schema
     |> addDbScriptExecution initEventProcessingDb
     |> addDbScriptExecution initOrderingDb
 
+let private configureGiraffe (services: IServiceCollection) =
+    services
+        .AddGiraffe()
+        .AddSingleton<Json.ISerializer>(
+            Func<IServiceProvider, Json.ISerializer>(fun sp ->
+                let jsonOptions = sp.GetRequiredService<JsonSerializerOptions>()
+                Json.FsharpFriendlySerializer(null, jsonOptions))
+        )
+
 let private configureServices (config: IConfiguration) (services: IServiceCollection) =
-    services.AddProblemDetails().AddGiraffe() |> configureDb config |> ignore
+    services.AddProblemDetails().AddGiraffe()
+    |> configureSerialization
+    |> configureDb config
+    |> ignore
 
 let configureBuilder (builder: WebApplicationBuilder) =
     let config = builder.Configuration
