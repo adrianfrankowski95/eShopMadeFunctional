5e22d9a1c1b3443195b3d370cf7b6d7facf5b7e9
Adrian Frankowski <adrian.frankowski95@gmail.com>
2025-03-21 17:34:30 +0100

    Remove .

--- src/eShop.RabbitMQ/Configuration.fs
+++ src/eShop.RabbitMQ/Configuration.fs
@@ -3,32 +3,32 @@
 open Microsoft.Extensions.Configuration
 
 let private getConnectionConfig (configuration: IConfiguration) =
-    { HostName = configuration.["RabbitMQ:Connection:HostName"]
-      Port = int (configuration.["RabbitMQ:Connection:Port"])
-      UserName = configuration.["RabbitMQ:Connection:UserName"]
-      Password = configuration.["RabbitMQ:Connection:Password"]
-      VirtualHost = configuration.["RabbitMQ:Connection:VirtualHost"]
-      SslEnabled = bool.Parse(configuration.["RabbitMQ:Connection:SslEnabled"]) }
+    { HostName = configuration["RabbitMQ:Connection:HostName"]
+      Port = int (configuration["RabbitMQ:Connection:Port"])
+      UserName = configuration["RabbitMQ:Connection:UserName"]
+      Password = configuration["RabbitMQ:Connection:Password"]
+      VirtualHost = configuration["RabbitMQ:Connection:VirtualHost"]
+      SslEnabled = bool.Parse(configuration["RabbitMQ:Connection:SslEnabled"]) }
 
 let private getConsumerConfig (configuration: IConfiguration) (serviceName: string) =
     { QueueName = serviceName
-      ExchangeName = configuration.["RabbitMQ:Consumer:ExchangeName"]
+      ExchangeName = configuration["RabbitMQ:Consumer:ExchangeName"]
       DeadLetterExchange =
-        match configuration.["RabbitMQ:Consumer:DeadLetterExchange"] with
+        match configuration["RabbitMQ:Consumer:DeadLetterExchange"] with
         | null -> None
         | value -> Some value
-      RetryCount = int configuration.["RabbitMQ:Consumer:RetryCount"]
-      PrefetchCount = uint16 (int (configuration.["RabbitMQ:Consumer:PrefetchCount"])) }
+      RetryCount = int configuration["RabbitMQ:Consumer:RetryCount"]
+      PrefetchCount = uint16 (int (configuration["RabbitMQ:Consumer:PrefetchCount"])) }
 
 let private getPublisherConfig (configuration: IConfiguration) =
-    { ExchangeName = configuration.["RabbitMQ:Publisher:ExchangeName"]
+    { ExchangeName = configuration["RabbitMQ:Publisher:ExchangeName"]
       // Default to "direct" if not specified in configuration
       ExchangeType =
-        match configuration.["RabbitMQ:Publisher:ExchangeType"] with
+        match configuration["RabbitMQ:Publisher:ExchangeType"] with
         | null -> "direct"
         | value -> value
-      Durable = bool.Parse(configuration.["RabbitMQ:Publisher:Durable"])
-      AutoDelete = bool.Parse(configuration.["RabbitMQ:Publisher:AutoDelete"]) }
+      Durable = bool.Parse(configuration["RabbitMQ:Publisher:Durable"])
+      AutoDelete = bool.Parse(configuration["RabbitMQ:Publisher:AutoDelete"]) }
 
 let getConfig (configuration: IConfiguration) (serviceName: string) =
     { Connection = getConnectionConfig configuration
